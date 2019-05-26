55b4afc4194fc604f210093a92edeeba93ba4d09
mr-jp <jasonphoon@gmail.com>
2019-05-26 23:00:33 +0800

    Update avalon.sql

--- avalon.sql
+++ avalon.sql
@@ -1,22 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
--- https://www.phpmyadmin.net/
+-- version 4.5.1
+-- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1:3306
--- Generation Time: May 24, 2019 at 12:48 PM
--- Server version: 5.7.26
--- PHP Version: 7.2.18
+-- Host: 127.0.0.1
+-- Generation Time: May 26, 2019 at 04:59 PM
+-- Server version: 10.1.13-MariaDB
+-- PHP Version: 7.0.5
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `avalon`
@@ -28,20 +20,23 @@ SET time_zone = "+00:00";
 -- Table structure for table `game`
 --
 
-DROP TABLE IF EXISTS `game`;
-CREATE TABLE IF NOT EXISTS `game` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `game` (
+  `id` int(11) NOT NULL,
   `timestamp` varchar(50) NOT NULL,
-  `started` enum('0','1') NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+  `started` enum('0','1') NOT NULL DEFAULT '0',
+  `players` int(11) NOT NULL,
+  `percival` enum('0','1') NOT NULL DEFAULT '0',
+  `mordred` enum('0','1') NOT NULL DEFAULT '0',
+  `morgana` enum('0','1') NOT NULL DEFAULT '0',
+  `oberon` enum('0','1') NOT NULL DEFAULT '0'
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `game`
 --
 
-INSERT INTO `game` (`id`, `timestamp`, `started`) VALUES
-(1, '1558700540', '0');
+INSERT INTO `game` (`id`, `timestamp`, `started`, `players`, `percival`, `mordred`, `morgana`, `oberon`) VALUES
+(31, '1558807713', '0', 5, '1', '1', '0', '0');
 
 -- --------------------------------------------------------
 
@@ -49,15 +44,66 @@ INSERT INTO `game` (`id`, `timestamp`, `started`) VALUES
 -- Table structure for table `player`
 --
 
-DROP TABLE IF EXISTS `player`;
-CREATE TABLE IF NOT EXISTS `player` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `player` (
+  `id` int(11) NOT NULL,
   `name` varchar(100) NOT NULL,
-  `fk_game_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
+  `team` varchar(50) NOT NULL,
+  `role` varchar(50) NOT NULL,
+  `fk_game_id` int(11) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+--
+-- Dumping data for table `player`
+--
+
+INSERT INTO `player` (`id`, `name`, `team`, `role`, `fk_game_id`) VALUES
+(1, 'Jason', '', '', 26),
+(2, 'Jason', '', '', 27),
+(3, 'Jason', '', '', 28),
+(4, 'Bob', '', '', 28),
+(5, 'Bob', '', '', 28),
+(6, 'Jason', '', '', 29),
+(7, 'Bob', '', '', 29),
+(8, 'Charlie', '', '', 29),
+(9, 'Re', '', '', 29),
+(10, 'ASD', '', '', 29),
+(11, 'asd', '', '', 29),
+(12, 'asd', '', '', 29),
+(13, 'Jason', '', '', 30),
+(14, 'a', '', '', 30),
+(15, 'b', '', '', 30),
+(16, 'c', '', '', 30),
+(17, 'd', '', '', 30),
+(18, 'Jason', '', '', 31);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `game`
+--
+ALTER TABLE `game`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `player`
+--
+ALTER TABLE `player`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_game_id` (`fk_game_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `game`
+--
+ALTER TABLE `game`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+--
+-- AUTO_INCREMENT for table `player`
+--
+ALTER TABLE `player`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
