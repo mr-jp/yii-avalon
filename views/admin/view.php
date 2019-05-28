b7476835220537a00fe0de9a57e51b9a1d6aee8b
Jason Phoon <jasonphoon@gmail.com>
2019-05-28 11:18:20 +0800

    Images for roles

--- views/admin/view.php
+++ views/admin/view.php
@@ -6,7 +6,7 @@ use yii\widgets\DetailView;
 /* @var $this yii\web\View */
 /* @var $model app\models\Game */
 
-$this->title = $model->id;
+$this->title = 'Game: '.$model->id;
 $this->params['breadcrumbs'][] = ['label' => 'Games', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 \yii\web\YiiAsset::register($this);
@@ -16,7 +16,11 @@ $this->params['breadcrumbs'][] = $this->title;
     <h2>Players</h2>
     <ol>
         <?php foreach ($players as $player): ?>
-            <li><?= ucfirst($player->name) ?> - <?= ucfirst($player->role) ?></li>
+            <li>
+                <a href="<?= Yii::$app->urlManager->createUrl(['admin/role','id'=>$model->id, 'playerId'=>$player->id]) ?>">
+                    <?= ucfirst($player->name) ?> - <?= ucfirst($player->role) ?>
+                </a>
+            </li>
         <?php endforeach ?>
     </ol>
 </div>
